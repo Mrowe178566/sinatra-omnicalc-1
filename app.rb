0e2fe698d7a57939295fb6536879e70b0b95374e
Maia Rowe <mrowe7000@icloud.com>
2025-02-06 00:04:42 +0000

    app build

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+erb(:bye)
+end
+
+get("/square/new") do
+erb(:new_square_calc)
+end
+
+get("/") do 
+"
+<h1> Welcome to your Sinatra App!</h1>
+
+
+"
 end
